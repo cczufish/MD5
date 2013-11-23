917f2309f0ea0432e7380801bfa4968ce72d2bf9
cczufish <718780261@qq.com>
2013-11-23 09:26:08 +0800

    MD5加密

---
+++ MyMD5.h
@@ -0,0 +1,21 @@
+//
+//  MyMD5.h
+//  SpokenTraining
+//
+//  Created by YuShuHui on 13-8-12.
+//  Copyright (c) 2013年 PKU. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyMD5 : NSObject
+
+
+/*****
+ MD5 加密
+ *****/
+
++(NSString *) md5: (NSString *) inPutText ;
+
+
+@end
---
+++ MyMD5.m
@@ -0,0 +1,31 @@
+//
+//  MyMD5.m
+//  SpokenTraining
+//
+//  Created by YuShuHui on 13-8-12.
+//  Copyright (c) 2013年 PKU. All rights reserved.
+//
+
+#import "MyMD5.h"
+#import "CommonCrypto/CommonDigest.h"
+
+
+@implementation MyMD5
+
+
++(NSString *) md5: (NSString *) inPutText
+{
+    const char *cStr = [inPutText UTF8String];
+    unsigned char result[CC_MD5_DIGEST_LENGTH];
+    CC_MD5(cStr, strlen(cStr), result);
+    
+    return [[NSString stringWithFormat:@"%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X",
+             result[0], result[1], result[2], result[3],
+             result[4], result[5], result[6], result[7],
+             result[8], result[9], result[10], result[11],
+             result[12], result[13], result[14], result[15]
+             ] lowercaseString];
+}
+
+
+@end
